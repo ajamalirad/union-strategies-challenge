f4c7d9e6ccecb4294cfda1e3ef6fc623c07943cf
ajamalirad <aplasticlanguage@gmail.com>
2021-07-29 12:48:12 -0400

    added map

--- src/Map.js
+++ src/Map.js
@@ -1,14 +1,33 @@
-import { Loader } from "@googlemaps/js-api-loader";
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-const Map = () => {
-
+const Map = (props) => {
 
     return (
-    <div id="map">
-        map here
-    </div>
+        <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyCAM7T-jrcMbdqhk_DyjKvXY-x6O6zsxAU'}}
+          defaultCenter={props.center}
+          defaultZoom={props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+          />
+        </GoogleMapReact>
+      </div>
     )
+}
+
+Map.defaultProps = {
+    center: {
+        lat: 59.95,
+        lng: 30.33
+      },
+      zoom: 11
 }
 
 export default Map;
